61683ac70c50e7cb8803a36c6c7005172d4517d7
khushalpankhaniya <khushalpankhaniya2004@gmail.com>
2025-01-07 11:27:37 +0530

    Add files via upload

---
+++ periodic-Table/main.js
@@ -0,0 +1,482 @@
+    const elements = [
+        {
+            "atomic_number": 1,
+            "name": "Hydrogen",
+            "discovered_by": "Henry Cavendish"
+        },
+        {
+            "atomic_number": 2,
+            "name": "Helium",
+            "discovered_by": "Pierre Janssen"
+        },
+        {
+            "atomic_number": 3,
+            "name": "Lithium",
+            "discovered_by": "Johan August Arfwedson"
+        },
+        {
+            "atomic_number": 4,
+            "name": "Beryllium",
+            "discovered_by": "Louis Nicolas Vauquelin"
+        },
+        {
+            "atomic_number": 5,
+            "name": "Boron",
+            "discovered_by": "Joseph Louis Gay-Lussac"
+        },
+        {
+            "atomic_number": 6,
+            "name": "Carbon",
+            "discovered_by": "Ancient Egypt"
+        },
+        {
+            "atomic_number": 7,
+            "name": "Nitrogen",
+            "discovered_by": "Daniel Rutherford"
+        },
+        {
+            "atomic_number": 8,
+            "name": "Oxygen",
+            "discovered_by": "Carl Wilhelm Scheele"
+        },
+        {
+            "atomic_number": 9,
+            "name": "Fluorine",
+            "discovered_by": "André-Marie Ampère"
+        },
+        {
+            "atomic_number": 10,
+            "name": "Neon",
+            "discovered_by": "Morris Travers"
+        },
+        {
+            "atomic_number": 11,
+            "name": "Sodium",
+            "discovered_by": "Humphry Davy"
+        },
+        {
+            "atomic_number": 12,
+            "name": "Magnesium",
+            "discovered_by": "Joseph Black"
+        },
+        {
+            "atomic_number": 13,
+            "name": "Aluminium",
+            "discovered_by": null
+        },
+        {
+            "atomic_number": 14,
+            "name": "Silicon",
+            "discovered_by": "Jöns Jacob Berzelius"
+        },
+        {
+            "atomic_number": 15,
+            "name": "Phosphorus",
+            "discovered_by": "Hennig Brand"
+        },
+        {
+            "atomic_number": 16,
+            "name": "Sulfur",
+            "discovered_by": "Ancient China"
+        },
+        {
+            "atomic_number": 17,
+            "name": "Chlorine",
+            "discovered_by": "Carl Wilhelm Scheele"
+        },
+        {
+            "atomic_number": 18,
+            "name": "Argon",
+            "discovered_by": "Lord Rayleigh"
+        },
+        {
+            "atomic_number": 19,
+            "name": "Potassium",
+            "discovered_by": "Humphry Davy"
+        },
+        {
+            "atomic_number": 20,
+            "name": "Calcium",
+            "discovered_by": "Humphry Davy"
+        },
+        {
+            "atomic_number": 21,
+            "name": "Scandium",
+            "discovered_by": "Lars Fredrik Nilson"
+        },
+        {
+            "atomic_number": 22,
+            "name": "Titanium",
+            "discovered_by": "William Gregor"
+        },
+        {
+            "atomic_number": 23,
+            "name": "Vanadium",
+            "discovered_by": "Andrés Manuel del Río"
+        },
+        {
+            "atomic_number": 24,
+            "name": "Chromium",
+            "discovered_by": "Louis Nicolas Vauquelin"
+        },
+        {
+            "atomic_number": 25,
+            "name": "Manganese",
+            "discovered_by": "Torbern Olof Bergman"
+        },
+        {
+            "atomic_number": 26,
+            "name": "Iron",
+            "discovered_by": "Ancient Egypt"
+        },
+        {
+            "atomic_number": 27,
+            "name": "Cobalt",
+            "discovered_by": "Georg Brandt"
+        },
+        {
+            "atomic_number": 28,
+            "name": "Nickel",
+            "discovered_by": "Axel Fredrik Cronstedt"
+        },
+        {
+            "atomic_number": 29,
+            "name": "Copper",
+            "discovered_by": "Middle East"
+        },
+        {
+            "atomic_number": 30,
+            "name": "Zinc",
+            "discovered_by": "India"
+        },
+        {
+            "atomic_number": 31,
+            "name": "Gallium",
+            "discovered_by": "Lecoq de Boisbaudran"
+        },
+        {
+            "atomic_number": 32,
+            "name": "Germanium",
+            "discovered_by": "Clemens Winkler"
+        },
+        {
+            "atomic_number": 33,
+            "name": "Arsenic",
+            "discovered_by": "Bronze Age"
+        },
+        {
+            "atomic_number": 34,
+            "name": "Selenium",
+            "discovered_by": "Jöns Jacob Berzelius"
+        },
+        {
+            "atomic_number": 35,
+            "name": "Bromine",
+            "discovered_by": "Antoine Jérôme Balard"
+        },
+        {
+            "atomic_number": 36,
+            "name": "Krypton",
+            "discovered_by": "William Ramsay"
+        },
+        {
+            "atomic_number": 37,
+            "name": "Rubidium",
+            "discovered_by": "Robert Bunsen"
+        },
+        {
+            "atomic_number": 38,
+            "name": "Strontium",
+            "discovered_by": "William Cruickshank"
+        },
+        {
+            "atomic_number": 39,
+            "name": "Yttrium",
+            "discovered_by": "Johan Gadolin"
+        },
+        {
+            "atomic_number": 40,
+            "name": "Zirconium",
+            "discovered_by": "Martin Heinrich Klaproth"
+        },
+        {
+            "atomic_number": 41,
+            "name": "Niobium",
+            "discovered_by": "Charles Hatchett"
+        },
+        {
+            "atomic_number": 42,
+            "name": "Molybdenum",
+            "discovered_by": "Carl Wilhelm Scheele"
+        },
+        {
+            "atomic_number": 43,
+            "name": "Technetium",
+            "discovered_by": "Carlo Perrier"
+        },
+        {
+            "atomic_number": 44,
+            "name": "Ruthenium",
+            "discovered_by": "Karl Ernst Claus"
+        },
+        {
+            "atomic_number": 45,
+            "name": "Rhodium",
+            "discovered_by": "William Hyde Wollaston"
+        },
+        {
+            "atomic_number": 46,
+            "name": "Palladium",
+            "discovered_by": "William Hyde Wollaston"
+        },
+        {
+            "atomic_number": 47,
+            "name": "Silver",
+            "discovered_by": "Middle East"
+        },
+        {
+            "atomic_number": 48,
+            "name": "Cadmium",
+            "discovered_by": "Karl Samuel Leberecht Hermann"
+        },
+        {
+            "atomic_number": 49,
+            "name": "Indium",
+            "discovered_by": "Ferdinand Reich"
+        },
+        {
+            "atomic_number": 50,
+            "name": "Tin",
+            "discovered_by": "Middle East"
+        },
+        {
+            "atomic_number": 51,
+            "name": "Antimony",
+            "discovered_by": "Bronze Age"
+        },
+        {
+            "atomic_number": 52,
+            "name": "Tellurium",
+            "discovered_by": "Franz-Joseph Müller von Reichenstein"
+        },
+        {
+            "atomic_number": 53,
+            "name": "Iodine",
+            "discovered_by": "Bernard Courtois"
+        },
+        {
+            "atomic_number": 54,
+            "name": "Xenon",
+            "discovered_by": "William Ramsay"
+        },
+        {
+            "atomic_number": 55,
+            "name": "Cesium",
+            "discovered_by": "Robert Bunsen"
+        },
+        {
+            "atomic_number": 56,
+            "name": "Barium",
+            "discovered_by": "Carl Wilhelm Scheele"
+        },
+        {
+            "atomic_number": 57,
+            "name": "Lanthanum",
+            "discovered_by": "Carl Gustaf Mosander"
+        },
+        {
+            "atomic_number": 58,
+            "name": "Cerium",
+            "discovered_by": "Martin Heinrich Klaproth"
+        },
+        {
+            "atomic_number": 59,
+            "name": "Praseodymium",
+            "discovered_by": "Carl Auer von Welsbach"
+        },
+        {
+            "atomic_number": 60,
+            "name": "Neodymium",
+            "discovered_by": "Carl Auer von Welsbach"
+        },
+        {
+            "atomic_number": 61,
+            "name": "Promethium",
+            "discovered_by": "Chien Shiung Wu"
+        },
+        {
+            "atomic_number": 62,
+            "name": "Samarium",
+            "discovered_by": "Lecoq de Boisbaudran"
+        },
+        {
+            "atomic_number": 63,
+            "name": "Europium",
+            "discovered_by": "Eugène-Anatole Demarçay"
+        },
+        {
+            "atomic_number": 64,
+            "name": "Gadolinium",
+            "discovered_by": "Jean Charles Galissard de Marignac"
+        },
+        {
+            "atomic_number": 65,
+            "name": "Terbium",
+            "discovered_by": "Carl Gustaf Mosander"
+        },
+        {
+            "atomic_number": 66,
+            "name": "Dysprosium",
+            "discovered_by": "Lecoq de Boisbaudran"
+        },
+        {
+            "atomic_number": 67,
+            "name": "Holmium",
+            "discovered_by": "Marc Delafontaine"
+        },
+        {
+            "atomic_number": 68,
+            "name": "Erbium",
+            "discovered_by": "Carl Gustaf Mosander"
+        },
+        {
+            "atomic_number": 69,
+            "name": "Thulium",
+            "discovered_by": "Per Teodor Cleve"
+        },
+        {
+            "atomic_number": 70,
+            "name": "Ytterbium",
+            "discovered_by": "Jean Charles Galissard de Marignac"
+        },
+        {
+            "atomic_number": 71,
+            "name": "Lutetium",
+            "discovered_by": "Georges Urbain"
+        },
+        {
+            "atomic_number": 72,
+            "name": "Hafnium",
+            "discovered_by": "Dirk Coster"
+        },
+        {
+            "atomic_number": 73,
+            "name": "Tantalum",
+            "discovered_by": "Anders Gustaf Ekeberg"
+        },
+        {
+            "atomic_number": 74,
+            "name": "Tungsten",
+            "discovered_by": "Carl Wilhelm Scheele"
+        },
+        {
+            "atomic_number": 75,
+            "name": "Rhenium",
+            "discovered_by": "Masataka Ogawa"
+        },
+        {
+            "atomic_number": 76,
+            "name": "Osmium",
+            "discovered_by": "Smithson Tennant"
+        },
+        {
+            "atomic_number": 77,
+            "name": "Iridium",
+            "discovered_by": "Smithson Tennant"
+        },
+        {
+            "atomic_number": 78,
+            "name": "Platinum",
+            "discovered_by": "Antonio de Ulloa"
+        },
+        {
+            "atomic_number": 79,
+            "name": "Gold",
+            "discovered_by": "Middle East"
+        },
+        {
+            "atomic_number": 80,
+            "name": "Mercury",
+            "discovered_by": "Ancient China"
+        },
+        {
+            "atomic_number": 81,
+            "name": "Thallium",
+            "discovered_by": "William Crookes"
+        },
+        {
+            "atomic_number": 82,
+            "name": "Lead",
+            "discovered_by": "Middle East"
+        },
+        {
+            "atomic_number": 83,
+            "name": "Bismuth",
+            "discovered_by": "Claude François Geoffroy"
+        },
+        {
+            "atomic_number": 84,
+            "name": "Polonium",
+            "discovered_by": "Marie Curie"
+        },
+        {
+            "atomic_number": 85,
+            "name": "Astatine",
+            "discovered_by": "Dale R. Corson"
+        },
+        {
+            "atomic_number": 86,
+            "name": "Radon",
+            "discovered_by": "Friedrich Ernst Dorn"
+        },
+        {
+            "atomic_number": 87,
+            "name": "Francium",
+            "discovered_by": "Marguerite Perey"
+        },
+        {
+            "atomic_number": 88,
+            "name": "Radium",
+            "discovered_by": "Pierre Curie"
+        },
+        {
+            "atomic_number": 89,
+            "name": "Actinium",
+            "discovered_by": "Friedrich Oskar Giesel"
+        },
+        {
+            "atomic_number": 90,
+            "name": "Thorium",
+            "discovered_by": "Jöns Jacob Berzelius"
+        },
+    ];
+
+    
+    // Function to create element cards
+    function createElementCard(element) {
+        var card = document.createElement('div');
+        card.classList.add('element');
+        card.innerHTML = `
+        <h2> atomic_number : ${element.atomic_number}</h2>
+        `;
+
+        card.setAttribute('name' , element.name);
+        card.setAttribute('discovered_by' , element.discovered_by);
+        return card;
+    }
+
+    // Display elements
+    var elementsContainer = document.querySelector('.elements');
+    elements.forEach(function (element) {
+        var elementCard = createElementCard(element);
+        elementsContainer.appendChild(elementCard);
+    });
+
+    elementsContainer.addEventListener('click', (e) => {
+        if (e.target.classList.contains('element')) {
+        const name = e.target.getAttribute("name");
+        const discovered_by = e.target.getAttribute("discovered_by");
+
+        alert(`Name: ${name}\nDiscoverd_by: ${discovered_by}`);
+        }
+    });
